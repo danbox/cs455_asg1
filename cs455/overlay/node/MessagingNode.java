af7c3cee483d373c56bb8b8c7a188dcecb2f7249
Dan Boxler <danbox19@msn.com>
2014-02-18 19:40:11 -0700

    testing send message

--- cs455/overlay/node/MessagingNode.java
+++ cs455/overlay/node/MessagingNode.java
@@ -274,7 +274,7 @@ public class MessagingNode implements Node
 		}
 		
 		//remove self from path
-		path.poll();
+//		path.poll();
 		System.out.println("Destination: " + path.getLast());
 		//get next node in path
 		Vertex next = path.element();
